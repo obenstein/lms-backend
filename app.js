bb36ede3eb21d6ff4b94c15ca41134316420915d
obiwankenobidev <obaid.rehman@ahya.ai>
2025-12-01 16:13:10 +0500

    mongodb atlas connected and added wild card to allowed origins

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(Express.json());
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
